4a9cf401136960280f78fe00b41b00fae913c7ef
sungyun <sungyun5423@gmail.com>
2022-08-15 13:40:51 +0900

    chore: install axios, react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 import BasicLayout from '~/components/layout/BasicLayout';
 import GlobalStyle from '~/styles/GlobalStyle';
 
 import type { AppProps } from 'next/app';
 
+const queryClient = new QueryClient();
+
 const _app = ({ Component, pageProps }: AppProps) => {
   return (
-    <BasicLayout>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </BasicLayout>
+    <QueryClientProvider client={queryClient}>
+      <BasicLayout>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </BasicLayout>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 };
 

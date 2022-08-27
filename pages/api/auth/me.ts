b9ec9be1ca2cae38965a0c2a76a5ea4b7773ee43
sungyun <sungyun5423@gmail.com>
2022-08-27 17:06:25 +0900

    feat: refresh token 으로 access token 갱신

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -1,4 +1,4 @@
-import { validateToken } from '~/lib/tokens';
+import { generateToken, validateToken } from '~/lib/tokens';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 
@@ -11,7 +11,19 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
       return res.status(200).json({ username, password });
     } else {
-      return res.status(200).json(undefined);
+      if (req.cookies.refresh_token) {
+        const { username, password, tokenId } = await validateToken(req.cookies.refresh_token);
+        const accessToken = await generateToken({ type: 'access_token', username, password, tokenId });
+
+        res.setHeader('Set-Cookie', [
+          `access_token=${accessToken}; httpOnly; path=/; secure; expires=${new Date(
+            Date.now() + 1000 * 60 * 60,
+          ).toUTCString()};`,
+        ]);
+        return res.status(200).json({ username, password });
+      } else {
+        return res.status(200).json(undefined);
+      }
     }
   } catch {
     return res.status(500).json({ error: 'Internal Server Error' });

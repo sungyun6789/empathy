86f64cc681cd8dd289a233040b7f81f1e7ab93dc
sungyun <sungyun5423@gmail.com>
2022-08-28 19:10:57 +0900

    feat: write page routing

--- components/write/WriteButton.tsx
+++ components/write/WriteButton.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link';
+
 import Button from '../system/Button';
 
 const WriteButton = () => {
-  return <Button>글쓰기</Button>;
+  return (
+    <Link href="/write">
+      <a>
+        <Button>글쓰기</Button>
+      </a>
+    </Link>
+  );
 };
 
 export default WriteButton;

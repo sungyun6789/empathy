388e403b0b0e399b068fd516f957cdb4c6b6730d
sungyun <sungyun5423@gmail.com>
2022-08-30 22:31:26 +0900

    feat: 쿠키가 존재할 때만 me 요청

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,36 @@ import CardList from '~/components/home/CardList';
 import { UserContext } from '~/contexts/UserContext';
 import { getMyAccount } from '~/lib/auth';
 
-const Home = () => {
+import type { NextPageContext } from 'next';
+
+interface Props {
+  accessToken: string | null;
+  refreshToken: string | null;
+}
+
+const Home = ({ accessToken, refreshToken }: Props) => {
   const { setState } = useContext(UserContext);
 
   useQuery(['/auth/me'], getMyAccount, {
     onSuccess: (data) => setState(data),
+    enabled: !!(accessToken || refreshToken),
   });
 
   return <CardList />;
 };
 
 export default Home;
+
+export async function getServerSideProps(context: NextPageContext) {
+  const cookies = context.req?.headers.cookie?.split('; ');
+
+  const accessToken = cookies?.[0].split('access_token=')[1] ?? null;
+  const refreshToken = cookies?.[1].split('refresh_token=')[1] ?? null;
+
+  return {
+    props: {
+      accessToken,
+      refreshToken,
+    },
+  };
+}

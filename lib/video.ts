259f140875e6f627fe93f31eb663542f8816fda9
sungyun <sungyun5423@gmail.com>
2022-09-05 21:52:35 +0900

    feat: 재생목록 안에 있는 영상 주소 파싱

--- lib/video.ts
+++ lib/video.ts
@@ -1,1 +1,8 @@
-export const videoIdParser = (url: string) => url.split('watch?v=')[1];
+export const videoIdParser = (url: string) => {
+  /** 재생목록 안에 있는 주소 파싱 */
+  if (url.includes('&list')) {
+    return url.split('&list')[0].split('watch?v=')[1];
+  } else {
+    return url.split('watch?v=')[1];
+  }
+};

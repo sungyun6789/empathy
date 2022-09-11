ec291f96092efa28311649f38cc15b4f06f0911c
sungyun <sungyun5423@gmail.com>
2022-09-11 23:33:19 +0900

    feat: 유튜브 영상 연결

--- components/home/CardDetail.tsx
+++ components/home/CardDetail.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import { useQuery } from 'react-query';
+import styled from 'styled-components';
 
 import { getItemDetails } from '~/lib/api/items';
 
@@ -11,7 +12,35 @@ const CardDetail = () => {
     enabled: !!id,
   });
 
-  return <div>{query.id}</div>;
+  return (
+    <Block>
+      <div>
+        <Description>{data?.description}</Description>
+        <Video
+          src={`https://www.youtube.com/embed/${data?.videoId}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
+    </Block>
+  );
 };
 
 export default CardDetail;
+
+const Block = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Description = styled.p`
+  padding: 10px;
+`;
+
+const Video = styled.iframe`
+  width: 100vw;
+  height: 50vh;
+`;

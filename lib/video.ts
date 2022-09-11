b96f57630790a0e594ee11f8f7d48711e7e6c4c8
sungyun <sungyun5423@gmail.com>
2022-09-11 23:59:55 +0900

    feat: 유튜브 주소가 맞는지 확인하는 조건 추가

--- lib/video.ts
+++ lib/video.ts
@@ -1,8 +1,10 @@
 export const videoIdParser = (url: string) => {
-  /** 재생목록 안에 있는 주소 파싱 */
-  if (url.includes('&list')) {
-    return url.split('&list')[0].split('watch?v=')[1];
-  } else {
-    return url.split('watch?v=')[1];
+  if (url.startsWith('https://www.youtube.com/')) {
+    /** 재생목록 안에 있는 주소 파싱 */
+    if (url.includes('&list')) {
+      return url.split('&list')[0].split('watch?v=')[1];
+    } else {
+      return url.split('watch?v=')[1];
+    }
   }
 };

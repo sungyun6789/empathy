eaaa8aaeeb822005725fe830ebf9e555a0af981b
sungyun <sungyun5423@gmail.com>
2022-09-14 00:27:54 +0900

    style: initial style for LikeButton component

--- components/system/LikeButton.tsx
+++ components/system/LikeButton.tsx
@@ -1,5 +1,34 @@
+import Image from 'next/image';
+import styled from 'styled-components';
+
 const LikeButton = () => {
-  return <div>좋아요 버튼</div>;
+  return (
+    <Block>
+      <div>
+        <LikeIconWrapper>
+          <Image src="/like.svg" alt="like" width="17" height="17" color="red" />
+        </LikeIconWrapper>
+        <LikeCount>좋아요 123개</LikeCount>
+      </div>
+    </Block>
+  );
 };
 
 export default LikeButton;
+
+const Block = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+`;
+
+const LikeIconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const LikeCount = styled.div`
+  margin-top: 10px;
+`;

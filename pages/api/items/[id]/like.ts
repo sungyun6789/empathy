af15541cd8248972b051304af02f7a35a8ae2757
sungyun <sungyun5423@gmail.com>
2022-09-15 01:37:48 +0900

    refactor: 확장성을 고려한 리팩토링

--- pages/api/items/[id]/like.ts
+++ pages/api/items/[id]/like.ts
@@ -4,26 +4,25 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    if (req.method === 'POST') {
-      if (req.query.id && typeof req.query.id === 'string' && req.cookies.access_token) {
-        const item = await prisma.item.findUnique({
+    if (typeof req.query.id === 'string') {
+      const item = await prisma.item.findUnique({
+        where: {
+          id: +req.query.id,
+        },
+      });
+
+      if (req.method === 'POST' && item) {
+        await prisma.item.update({
           where: {
-            id: +req.query.id,
+            id: item.id,
+          },
+          data: {
+            like: item.like + 1,
           },
         });
 
-        if (item) {
-          await prisma.item.update({
-            where: {
-              id: item.id,
-            },
-            data: {
-              like: item.like + 1,
-            },
-          });
-        }
+        return res.status(200).json(undefined);
       }
-      return res.status(200).json(undefined);
     }
   } catch {
     return res.status(500).json({ error: 'Internal Server Error' });

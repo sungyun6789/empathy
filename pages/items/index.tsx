d6671046d0d38ebbd45e94bf0b1ac11769624579
sungyun <sungyun5423@gmail.com>
2022-09-14 20:23:18 +0900

    fix: 게시글 자세히 보기 페이지에서 로그아웃 풀리는 버그 수정

--- pages/items/index.tsx
+++ pages/items/index.tsx
@@ -1,7 +1,30 @@
 import CardDetail from '~/components/home/CardDetail';
+import useUser from '~/hooks/useUser';
+import { cookieParser } from '~/lib/cookies';
 
-const items = () => {
+import type { NextPageContext } from 'next';
+import type { CookieParserToken } from '~/lib/cookies';
+
+interface Props {
+  access_token: CookieParserToken['access_token'];
+  refresh_token: CookieParserToken['refresh_token'];
+}
+
+const items = ({ access_token, refresh_token }: Props) => {
+  useUser({ access_token, refresh_token });
+
   return <CardDetail />;
 };
 
 export default items;
+
+export async function getServerSideProps(context: NextPageContext) {
+  const { access_token, refresh_token } = cookieParser(context.req?.headers.cookie);
+
+  return {
+    props: {
+      access_token,
+      refresh_token,
+    },
+  };
+}

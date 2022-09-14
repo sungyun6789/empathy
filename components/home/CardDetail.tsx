778307cd14b2c34a037cc42f59ce9353c9e4d1b5
sungyun <sungyun5423@gmail.com>
2022-09-15 01:39:21 +0900

    feat: refetch를 위해 LikeButton 기능을 CardDetail Component에서 관리하도록 함

--- components/home/CardDetail.tsx
+++ components/home/CardDetail.tsx
@@ -1,17 +1,20 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import styled from 'styled-components';
 
-import { getItemDetails } from '~/lib/api/items';
-
-import LikeButton from '../system/LikeButton';
+import { getItemDetails, likeItem } from '~/lib/api/items';
 
 const CardDetail = () => {
   const { query } = useRouter();
   const id = query.id as string;
 
-  const { data } = useQuery([`/items/${id}`], () => getItemDetails(id), {
+  const { data, refetch } = useQuery([`/items/${id}`], () => getItemDetails(id), {
     enabled: !!id,
+  });
+
+  const { mutate } = useMutation(likeItem, {
+    onSuccess: () => refetch(),
   });
 
   return (
@@ -28,7 +31,14 @@ const CardDetail = () => {
           />
         </div>
       </Block>
-      <LikeButton like={data?.like} />
+      <LikeButton>
+        <div>
+          <LikeIconWrapper onClick={() => mutate(id)}>
+            <Image src="/like.svg" alt="like" width="17" height="17" />
+          </LikeIconWrapper>
+          <LikeCount>좋아요 {data?.like}개</LikeCount>
+        </div>
+      </LikeButton>
     </>
   );
 };
@@ -48,4 +58,21 @@ const Description = styled.p`
 const Video = styled.iframe`
   width: 100vw;
   height: 50vh;
+`;
+
+const LikeButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+`;
+
+const LikeIconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const LikeCount = styled.div`
+  margin-top: 10px;
 `;
